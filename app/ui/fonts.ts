15eb360d714a5ee102b00ff3c701e3dcbda08bcf
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2024-06-10 11:48:15 +0300

    Add Coustom fonts (primary & secondery fonts)

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+// console.log(inter);
+export const lusitana = Lusitana({ subsets: ['latin'], weight: '700' });
